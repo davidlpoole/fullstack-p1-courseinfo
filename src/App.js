7b74e80909f6bc9e943cd9eb4300afa1ff77d9b5
David Poole <14233215+davidlpoole@users.noreply.github.com>
2021-09-09 15:32:18 +1200

    refactor to header, content, footer components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
 import React from 'react'
 
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const ex1 = 10
+  const part2 = 'Using props to pass data'
+  const ex2 = 7
+  const part3 = 'State of a component'
+  const ex3 = 14
+
+  const Header = (props) => {
+    return (
+      <h1>
+        {props.course}
+      </h1>
+    )
+  }
+
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part} {props.ex}
+      </p>
+    )
+  }
+
+  const Content = () => {
+    return (
+    <>
+      <Part part={part1} ex={ex1} />
+      <Part part={part2} ex={ex2} />
+      <Part part={part3} ex={ex3} />
+    </>
+    )
+  }
+
+  const Total = () => {
+    return (
+    <p>
+        Number of exercises {ex1 + ex2 + ex3}
+    </p>
+    )
+  }
+
   return (
-    <div className="App">
-      test
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total />
     </div>
   );
 }
